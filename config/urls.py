91a5b9f40784d82fd783aa46358b20d5bb11bf9b
Gseungjin2 <jchsekfr5@gmail.com>
2024-08-27 15:34:05 +0900

    ✨feat: chapter_03 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import Http404
 from django.shortcuts import render
 from fake_db import user_db
 from todo.views import todo_list, todo_info
+from users import views as user_views
 
 def user_list(request):
     names = [{'id': key, 'name': value['이름']} for key, value in user_db.items()]
@@ -39,5 +40,8 @@ urlpatterns = [
     path('users/<int:user_id>/', user_info, name='user_info'),
     path('todo/', todo_list, name='todo_list'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', user_views.login, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='signup')
 ]
 
